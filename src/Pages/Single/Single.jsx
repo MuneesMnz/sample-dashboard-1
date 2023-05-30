e49636ae6b40b0287f72a5ef1151838b1ab4982e
mnz <muneeskarakkad62@gmail.com>
2023-05-30 19:12:07 +0530

    single product Data Shown Container Completed

--- src/Pages/Single/Single.jsx
+++ src/Pages/Single/Single.jsx
@@ -1,11 +1,58 @@
-import React from 'react'
+import React from "react";
+import Sidebar from "../../components/Sidebar/Sidebar";
+import Navbar from "../../components/Navbar/Navbar";
 
 const Single = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <div className="flex w-full">
+      <Sidebar />
+      <div className="flex-[6]">
+        <Navbar />
+        <div className="p-5 flex g-5">
+          <div className="flex-1 shadow-md p-5 relative">
+            <div className="text-xl font font-semibold mb-3 text-gray-500">Information</div>
+            <div className="flex items-center gap-5 ">
+              <img
+                src="https://m.media-amazon.com/images/I/7196oQDyF6L.jpg"
+                alt=""
+                className="w-[100px] h-[100px] object-cover "
+              />
+              <div className="absolute top-5 right-5 p-1 text-sm text-blue-800 bg-blue-200 border-blue-800  rounded cursor-pointer">
+                edit
+              </div>
 
-export default Single
+              <div>
+                <h1 className="text-4xl font-semibold mb-2">
+                  Lays{" "}
+                  <span className="text-[16px] text-gray-500">(PepsiCo)</span>
+                </h1>
+                <div className="mb-2 text-[14px]">
+                  <span className="font-semibold text-gray-500 mr-1">
+                    Price:
+                  </span>
+                  <span>30rs per 1packet</span>
+                </div>
+                <div className="mb-2 text-[14px]">
+                  <span className="font-semibold text-gray-500 mr-1">
+                    Category:
+                  </span>
+                  <span>Food</span>
+                </div>
+                <div className="mb-2 text-[14px]">
+                  <span className="font-semibold text-gray-500 mr-1">
+                    Stock:
+                  </span>
+                  <span>15 Available</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="flex-[2]"></div>
+        </div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default Single;
